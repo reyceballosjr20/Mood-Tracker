aa8a6703f5d73ac779f6a0c9a37fce5058641bbf
Rey <reynaldoceballosjr02@gmail.com>
2025-03-19 14:38:29 +0800

    a

--- config/oauth.php
+++ config/oauth.php
@@ -7,6 +7,6 @@
 
 // Google OAuth settings
 // IMPORTANT: Replace these values with your actual Google Cloud Console credentials
-define('GOOGLE_CLIENT_ID', 'YOUR_GOOGLE_CLIENT_ID'); // From Google Cloud Console
-define('GOOGLE_CLIENT_SECRET', 'YOUR_GOOGLE_CLIENT_SECRET'); // From Google Cloud Console
-define('GOOGLE_REDIRECT_URI', 'http://localhost/Mood-Tracker/dist/google-callback.php'); // Must match exactly what you set in Google Cloud Console 
+define('GOOGLE_CLIENT_ID', '684152867710-1a92h1mu8iknhdhhn7o8ngq1vds10u83.apps.googleusercontent.com'); // From Google Cloud Console
+define('GOOGLE_CLIENT_SECRET', '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'); // From Google Cloud Console
+define('GOOGLE_REDIRECT_URI', 'http://localhost/Mood-Tracker/dist/login.php'); // Must match exactly what you set in Google Cloud Console 
